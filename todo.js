49a00e0d0c579056ee9fa5716179a31d9e0eb723
Allen B <pingguo.ny@gmail.com>
2018-12-17 20:19:24 -0500

    refactoring

--- todo.js
+++ todo.js
@@ -1,16 +1,76 @@
-function Task(name, context, project) {
-	this.name = name
-	this.context = context
-	this.project = project
-	this.metadata = {}
+var Task = Object.create(null)
+
+
+var TodoList = function() {
+	/* protected */
+	this.list = []
 }
 
-Task.prototype.due = function() {
-	str = this.metadata["due"]
-	if(typeof str !== "string")
-		return null
-	data = /^(\d{4})-(\d{2})-(\d{2})$/.exec(str)
-	if(data == null)
-		return null
-	return new Date(data[1] | 0, data[2] - 1, data[3] | 0)
+TodoList.prototype.add = function(task) {
+	this.list.push(task)
+	return this.list.length
+}
+
+TodoList.prototype.get = function(id) {
+	return this.list[id - 1]
+}
+
+TodoList.prototype.has = function(id) {
+	return (id - 1) in this.list
+}
+
+TodoList.prototype.set = function(id, task) {
+	this.list[id - 1] = task
+}
+
+TodoList.prototype.delete = function(id) {
+	delete this.list[id - 1]
+}
+
+TodoList.prototype.keys = function() {
+	var out = []
+	for (var i = 0; i < this.list.length; i++) {
+		if (i in this.list) {
+			out.push(i + 1)
+		}
+	}
+	return out
+}
+
+TodoList.prototype.toString = function() {
+	return this.list.join("\n")
+}
+
+var TodoFile = function() {
+	TodoList.call(this)
+}
+TodoFile.prototype = Object.create(TodoList.prototype)
+
+TodoFile.prototype.pull = function() {
+	throw new Error("Pull: Not implemented")
+}
+
+TodoFile.prototype.push = function() {
+	throw new Error("Push: Not implemented")
+}
+TodoFile.prototype.filename = null
+
+var LocalTodoFile = function(name) {
+	TodoFile.call(this)
+	this.filename = name
+}
+LocalTodoFile.prototype = Object.create(TodoFile.prototype)
+
+LocalTodoFile.prototype.pull = function() {
+	this.list = String(localStorage.getItem(this.filename)).split("\n")
+	var self = this
+	this.list.forEach(function(item, index) {
+		if (item.length === 0) {
+			delete self.list[index]
+		}
+	})
+}
+
+LocalTodoFile.prototype.push = function() {
+	localStorage.setItem(this.filename, this.toString())
 }
